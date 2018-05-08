99cd49729089bb9bdf937c47d1d59cc067222376
tienph <tienph@rikkeisoft.com>
2018-05-08 10:30:41 +0700

    @TableGenerator

--- TableGenerator/src/main/java/com/supportmycode/dao/Main.java
+++ TableGenerator/src/main/java/com/supportmycode/dao/Main.java
@@ -8,17 +8,16 @@ import com.supportmycode.persistence.HibernateUtil;
 
 public class Main {
 
-	@SuppressWarnings("unchecked")
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SessionFactory sf = HibernateUtil.getSessionFactory();
-		Session session = sf.openSession();
-		session.beginTransaction();		
-		
-		Employee emp1 = new Employee("Nina", "Mayers", "111");		
-				
-		session.save(emp1);		
-		session.getTransaction().commit();
-		session.close();
-	}
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.openSession();
+        session.beginTransaction();
+
+        Employee emp1 = new Employee("Nina", "Mayers", "111");
+
+        session.save(emp1);
+        session.getTransaction().commit();
+        session.close();
+    }
 }
--- TableGenerator/src/main/java/com/supportmycode/model/Employee.java
+++ TableGenerator/src/main/java/com/supportmycode/model/Employee.java
@@ -10,82 +10,77 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.TableGenerator;
 
-
 @Entity
-@Table(name="EMPLOYEE")
+@Table(name = "EMPLOYEE")
 public class Employee {
 
-	@Id
-	@GeneratedValue(strategy=GenerationType.TABLE , generator="employee_generator")
-	@TableGenerator(name="employee_generator", 
-					table="pk_table", 
-					pkColumnName="name", 
-					valueColumnName="vlaue",        					
-					allocationSize=100)	
-	@Column(name="employee_id")
-	private Long employeeId;
-	
-	@Column(name="firstname")
-	private String firstname;
-	
-	@Column(name="lastname")
-	private String lastname;
-	
-	@Column(name="birth_date")
-	private Date birthDate;
-	
-	@Column(name="cell_phone")
-	private String cellphone;
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "employee_generator")
+    @TableGenerator(name = "employee_generator", table = "pk_table", pkColumnName = "name", valueColumnName = "vlaue", allocationSize = 100)
+    @Column(name = "employee_id")
+    private Long employeeId;
 
-	public Employee() {
-		
-	}
-	
-	public Employee(String firstname, String lastname, String phone) {
-		this.firstname = firstname;
-		this.lastname = lastname;
-		this.birthDate = new Date(System.currentTimeMillis());
-		this.cellphone = phone;
-	}
+    @Column(name = "firstname")
+    private String firstname;
 
-	public Long getEmployeeId() {
-		return employeeId;
-	}
+    @Column(name = "lastname")
+    private String lastname;
 
-	public void setEmployeeId(Long employeeId) {
-		this.employeeId = employeeId;
-	}
+    @Column(name = "birth_date")
+    private Date birthDate;
 
-	public String getFirstname() {
-		return firstname;
-	}
+    @Column(name = "cell_phone")
+    private String cellphone;
 
-	public String getLastname() {
-		return lastname;
-	}
+    public Employee() {
 
-	public Date getBirthDate() {
-		return birthDate;
-	}
+    }
 
-	public String getCellphone() {
-		return cellphone;
-	}
+    public Employee(String firstname, String lastname, String phone) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.birthDate = new Date(System.currentTimeMillis());
+        this.cellphone = phone;
+    }
 
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
+    public Long getEmployeeId() {
+        return employeeId;
+    }
 
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
 
-	public void setBirthDate(Date birthDate) {
-		this.birthDate = birthDate;
-	}
+    public String getFirstname() {
+        return firstname;
+    }
 
-	public void setCellphone(String cellphone) {
-		this.cellphone = cellphone;
-	}
-	
+    public String getLastname() {
+        return lastname;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public String getCellphone() {
+        return cellphone;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setCellphone(String cellphone) {
+        this.cellphone = cellphone;
+    }
+
 }
